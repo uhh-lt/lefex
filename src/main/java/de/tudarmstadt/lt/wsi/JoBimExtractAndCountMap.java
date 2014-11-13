b9189a94badca34829a54f4924cf7c226274f54c
Johannes Simon <johannes.simon@mail.de>
2014-11-13 16:27:14 +0100

    - optimize UIMA pipeline execution

--- src/main/java/de/tudarmstadt/lt/wsi/JoBimExtractAndCountMap.java
+++ src/main/java/de/tudarmstadt/lt/wsi/JoBimExtractAndCountMap.java
@@ -13,7 +13,6 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.log4j.Logger;
 import org.apache.uima.analysis_engine.AnalysisEngine;
-import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.cas.CASException;
 import org.apache.uima.fit.factory.AnalysisEngineFactory;
 import org.apache.uima.fit.util.JCasUtil;
@@ -31,34 +30,32 @@ import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordLemmatizer;
 
 class JoBimExtractAndCountMap extends Mapper<LongWritable, Text, Text, IntWritable> {
 	Logger log = Logger.getLogger("de.tudarmstadt.lt.wsi");
-	AnalysisEngine engine;
+	AnalysisEngine segmenter;
+	AnalysisEngine posTaggerLemmatizer;
+	AnalysisEngine depParser;
 	JCas jCas;
 	boolean semantifyDependencies;
+	boolean computeDependencies;
+	boolean computeCoocs;
 	
 	private static IntWritable ONE = new IntWritable(1);
-
-	public AnalysisEngineDescription buildAnalysisEngine() throws ResourceInitializationException {
-		AnalysisEngineDescription segmenter = AnalysisEngineFactory.createEngineDescription(OpenNlpSegmenter.class);
-		AnalysisEngineDescription pos = AnalysisEngineFactory.createEngineDescription(OpenNlpPosTagger.class);
-		AnalysisEngineDescription lemmatizer = AnalysisEngineFactory.createEngineDescription(StanfordLemmatizer.class);
-		AnalysisEngineDescription deps = AnalysisEngineFactory.createEngineDescription(MaltParser.class);
-
-		return AnalysisEngineFactory.createEngineDescription(
-				segmenter, pos, lemmatizer, deps);
-	}
 	
 	@Override
 	public void setup(Context context) {
 		log.info("Initializing JoBimExtractAndCount...");
 		try {
-			engine = AnalysisEngineFactory.createEngine(buildAnalysisEngine());
+			segmenter = AnalysisEngineFactory.createEngine(OpenNlpSegmenter.class);
+			posTaggerLemmatizer = AnalysisEngineFactory.createEngine(OpenNlpPosTagger.class, StanfordLemmatizer.class);
+			depParser = AnalysisEngineFactory.createEngine(MaltParser.class);
 			jCas = CasCreationUtils.createCas(createTypeSystemDescription(), null, null).getJCas();
 		} catch (ResourceInitializationException e) {
 			log.error("Couldn't initialize analysis engine", e);
 		} catch (CASException e) {
 			log.error("Couldn't create new CAS", e);
 		}
-		semantifyDependencies = context.getConfiguration().getBoolean("dependencies.semantify", false);
+		computeCoocs = context.getConfiguration().getBoolean("holing.coocs", false);
+		computeDependencies = context.getConfiguration().getBoolean("holing.dependencies", false);
+		semantifyDependencies = context.getConfiguration().getBoolean("holing.dependencies.semantify", false);
 		log.info("Ready!");
 	}
 	
@@ -70,7 +67,7 @@ class JoBimExtractAndCountMap extends Mapper<LongWritable, Text, Text, IntWritab
 			jCas.reset();
 			jCas.setDocumentText(text);
 			jCas.setDocumentLanguage("en");
-			engine.process(jCas);
+			segmenter.process(jCas);
 			Collection<Token> tokens = JCasUtil.select(jCas, Token.class);
 			Set<String> tokenSet = new HashSet<String>();
 			final int MAX_NUM_TOKENS = 100;
@@ -78,52 +75,61 @@ class JoBimExtractAndCountMap extends Mapper<LongWritable, Text, Text, IntWritab
 				context.getCounter("de.tudarmstadt.lt.wsi", "NUM_SKIPPED_SENTENCES").increment(1);
 				return;
 			}
-			for (Token token : tokens) {
-				String lemma = token.getLemma().getValue();
-				tokenSet.add(lemma);
+
+			posTaggerLemmatizer.process(jCas);
+			if (computeCoocs) {
+				for (Token token : tokens) {
+					String lemma = token.getLemma().getValue();
+					tokenSet.add(lemma);
+				}
+				
+				for (String lemma : tokenSet) {
+					context.write(new Text("CoocF\t" + lemma), ONE);
+				}
+				
+				for (Token token : tokens) {
+					String pos = token.getPos().getPosValue();
+					String lemma = token.getLemma().getValue();
+					context.write(new Text("W\t" + lemma), ONE);
+					if (pos.equals("NN") || pos.equals("NNS")) {
+						for (String lemma2 : tokenSet) {
+							context.write(new Text("CoocWF\t" + lemma + "\t" + lemma2), ONE);
+						}
+					}
+					context.progress();
+				}
 			}
 			
-			for (String lemma : tokenSet) {
-				context.write(new Text("CoocF\t" + lemma), ONE);
-			}
-			
-			for (Token token : tokens) {
-				String pos = token.getPos().getPosValue();
-				String lemma = token.getLemma().getValue();
-				context.write(new Text("W\t" + lemma), ONE);
-				if (pos.equals("NN") || pos.equals("NNS")) {
-					for (String lemma2 : tokenSet) {
-						context.write(new Text("CoocWF\t" + lemma + "\t" + lemma2), ONE);
+			if (computeDependencies) {
+				depParser.process(jCas);
+				Collection<Dependency> deps = JCasUtil.select(jCas, Dependency.class);
+				for (Dependency dep : deps) {
+					Token source = dep.getGovernor();
+					Token target = dep.getDependent();
+					String rel = dep.getDependencyType();
+					if (semantifyDependencies) {
+						rel = semantifyDependencyRelation(rel);
+						if (rel == null) {
+							continue;
+						}
 					}
-				}
-				context.progress();
-			}
-			Collection<Dependency> deps = JCasUtil.select(jCas, Dependency.class);
-			for (Dependency dep : deps) {
-				Token source = dep.getGovernor();
-				Token target = dep.getDependent();
-				String rel = dep.getDependencyType();
-				if (semantifyDependencies) {
-					rel = semantifyDependencyRelation(rel);
-					if (rel == null) {
-						continue;
+					String sourcePos = source.getPos().getPosValue();
+					String targetPos = target.getPos().getPosValue();
+					String sourceLemma = source.getLemma().getValue();
+					String targetLemma = target.getLemma().getValue();
+					if (sourcePos.equals("NN") || sourcePos.equals("NNS")) {
+						String bim = rel + "(@@," + targetLemma + ")";
+						context.write(new Text("DepF\t" + bim), ONE);
+						context.write(new Text("DepWF\t" + sourceLemma + "\t" + bim), ONE);
 					}
+					if (targetPos.equals("NN") || targetPos.equals("NNS")) {
+						String bim = rel + "(" + sourceLemma + ",@@)";
+						context.write(new Text("DepF\t" + bim), ONE);
+						context.write(new Text("DepWF\t" + targetLemma + "\t" + bim), ONE);
+					}
+					context.progress();
+					
 				}
-				String sourcePos = source.getPos().getPosValue();
-				String targetPos = target.getPos().getPosValue();
-				String sourceLemma = source.getLemma().getValue();
-				String targetLemma = target.getLemma().getValue();
-				if (sourcePos.equals("NN") || sourcePos.equals("NNS")) {
-					String bim = rel + "(@@," + targetLemma + ")";
-					context.write(new Text("DepF\t" + bim), ONE);
-					context.write(new Text("DepWF\t" + sourceLemma + "\t" + bim), ONE);
-				}
-				if (targetPos.equals("NN") || targetPos.equals("NNS")) {
-					String bim = rel + "(" + sourceLemma + ",@@)";
-					context.write(new Text("DepF\t" + bim), ONE);
-					context.write(new Text("DepWF\t" + targetLemma + "\t" + bim), ONE);
-				}
-				context.progress();
 			}
 		} catch (Exception e) {
 			log.error("Can't process line: " + value.toString(), e);
