a701b5172f15f712796ad774e0ee635e48f5cc60
Johannes Simon <johannes.simon@mail.de>
2014-12-15 16:20:49 +0100

    - use mate tools

--- src/main/java/de/tudarmstadt/lt/wsi/JoBimExtractAndCountMap.java
+++ src/main/java/de/tudarmstadt/lt/wsi/JoBimExtractAndCountMap.java
@@ -3,10 +3,12 @@ package de.tudarmstadt.lt.wsi;
 import static org.apache.uima.fit.factory.TypeSystemDescriptionFactory.createTypeSystemDescription;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.Set;
@@ -26,10 +28,10 @@ import org.apache.uima.util.CasCreationUtils;
 
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 import de.tudarmstadt.ukp.dkpro.core.api.syntax.type.dependency.Dependency;
-import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpPosTagger;
+import de.tudarmstadt.ukp.dkpro.core.matetools.MateLemmatizer;
+import de.tudarmstadt.ukp.dkpro.core.matetools.MateParser;
+import de.tudarmstadt.ukp.dkpro.core.matetools.MatePosTagger;
 import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpSegmenter;
-import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordLemmatizer;
-import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordParser;
 
 
 class JoBimExtractAndCountMap extends Mapper<LongWritable, Text, Text, IntWritable> {
@@ -58,10 +60,10 @@ class JoBimExtractAndCountMap extends Mapper<LongWritable, Text, Text, IntWritab
 		semantifyDependencies = context.getConfiguration().getBoolean("holing.dependencies.semantify", false);
 		try {
 			segmenter = AnalysisEngineFactory.createEngine(OpenNlpSegmenter.class);
-			posTagger = AnalysisEngineFactory.createEngine(OpenNlpPosTagger.class);
-			lemmatizer = AnalysisEngineFactory.createEngine(StanfordLemmatizer.class);
+			posTagger = AnalysisEngineFactory.createEngine(MatePosTagger.class);
+			lemmatizer = AnalysisEngineFactory.createEngine(MateLemmatizer.class);
 			if (computeDependencies) {
-				depParser = AnalysisEngineFactory.createEngine(StanfordParser.class);
+				depParser = AnalysisEngineFactory.createEngine(MateParser.class);
 			}
 			jCas = CasCreationUtils.createCas(createTypeSystemDescription(), null, null).getJCas();
 		} catch (ResourceInitializationException e) {
@@ -142,7 +144,8 @@ class JoBimExtractAndCountMap extends Mapper<LongWritable, Text, Text, IntWritab
 			if (computeDependencies) {
 				depParser.process(jCas);
 				Collection<Dependency> deps = JCasUtil.select(jCas, Dependency.class);
-				for (Dependency dep : deps) {
+				Collection<Dependency> depsCollapsed = collapseDependencies(jCas, deps, tokens);
+				for (Dependency dep : depsCollapsed) {
 					Token source = dep.getGovernor();
 					Token target = dep.getDependent();
 					String rel = dep.getDependencyType();
@@ -152,8 +155,14 @@ class JoBimExtractAndCountMap extends Mapper<LongWritable, Text, Text, IntWritab
 							continue;
 						}
 					}
-					String sourcePos = source.getPos().getPosValue();
-					String targetPos = target.getPos().getPosValue();
+					String sourcePos = null;
+					String targetPos = null;
+					try {
+						sourcePos = source.getPos().getPosValue();
+						targetPos = target.getPos().getPosValue();
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
 					String sourceLemma = tokenLemmas.get(source);//source.getLemma().getValue();
 					String targetLemma = tokenLemmas.get(target);//target.getLemma().getValue();
 					if (sourcePos.equals("NN") || sourcePos.equals("NNS")) {
@@ -174,6 +183,40 @@ class JoBimExtractAndCountMap extends Mapper<LongWritable, Text, Text, IntWritab
 			log.error("Can't process line: " + value.toString(), e);
 			context.getCounter("de.tudarmstadt.lt.wiki", "NUM_MAP_ERRORS").increment(1);
 		}
+	}
+	
+	private Collection<Dependency> collapseDependencies(JCas jCas, Collection<Dependency> deps, Collection<Token> tokens) {
+		List<Dependency> collapsedDeps = new ArrayList<>(deps);
+		for (Token token : tokens) {
+			if (token.getPos().getPosValue().equals("IN")) {
+				List<Dependency> toRemove = new ArrayList<>();
+				String depType = "prep_" + token.getCoveredText().toLowerCase();
+				Token source = null;
+				Token target = null;
+				int begin = -1;
+				int end = -1;
+				for (Dependency dep : collapsedDeps) {
+					if (dep.getGovernor() == token) {
+						end = dep.getEnd();
+						target = dep.getDependent();
+						toRemove.add(dep);
+					} else if (dep.getDependent() == token) {
+						begin = dep.getBegin();
+						source = dep.getGovernor();
+						toRemove.add(dep);
+					}
+				}
+				if (source != null && target != null) {
+					Dependency collapsedDep = new Dependency(jCas, begin, end);
+					collapsedDep.setGovernor(source);
+					collapsedDep.setDependent(target);
+					collapsedDep.setDependencyType(depType);
+					collapsedDeps.add(collapsedDep);
+					collapsedDeps.removeAll(toRemove);
+				}
+			}
+		}
+		return collapsedDeps;
 	}
 	
 	private String semantifyDependencyRelation(String rel) {
